6659c23caba43c3239da70f52450cfdd66e96da7
Alvin Lu <mmx15mxa@uea.ac.uk>
2019-06-10 22:04:55 +0100

    Update error message

--- FSMPrototype/src/fsmprototype/Play_Game.java
+++ FSMPrototype/src/fsmprototype/Play_Game.java
@@ -804,12 +804,7 @@ public class Play_Game extends javax.swing.JFrame {
             return true;
         }
         else{
-            Gameplay.append("No such object found");
-            JLabel label = new JLabel("Object not found!");
-            label.setFont(new Font("Tahoma", Font.PLAIN, 24));
-            JOptionPane.showMessageDialog(jPanel1, 
-                label, 
-                "Object not found", JOptionPane.WARNING_MESSAGE);
+            Gameplay.append("No such object found!\n");
         }
         return false;
     }
